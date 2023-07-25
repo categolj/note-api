fca1910cf5f19fb3c099d6f79ffe86176af69a5f
Toshiaki Maki <makingx@gmail.com>
2023-07-25 19:54:08 +0900

    Use RestTemplateAdapter instead of WebClientAdapter

--- src/main/java/am/ik/note/config/EntryClientConfig.java
+++ src/main/java/am/ik/note/config/EntryClientConfig.java
@@ -2,26 +2,24 @@ package am.ik.note.config;
 
 import am.ik.note.entry.EntryClient;
 import am.ik.note.entry.EntryProps;
-
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.client.support.RestTemplateAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(EntryProps.class)
 public class EntryClientConfig {
 	@Bean
-	public EntryClient entryClient(WebClient.Builder builder, EntryProps props) {
-		final WebClientAdapter adapter = WebClientAdapter
-				.forClient(builder.baseUrl(props.apiUrl())
-						.defaultHeaders(httpHeaders -> httpHeaders
-								.setBasicAuth(props.clientId(), props.clientSecret()))
-						.build());
-		final HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(adapter)
-				.build();
+	public EntryClient entryClient(RestTemplateBuilder restTemplateBuilder,
+			EntryProps props) {
+		final RestTemplateAdapter adapter = RestTemplateAdapter.create(restTemplateBuilder
+				.rootUri(props.apiUrl())
+				.basicAuthentication(props.clientId(), props.clientSecret()).build());
+		final HttpServiceProxyFactory factory = HttpServiceProxyFactory
+				.builderFor(adapter).build();
 		return factory.createClient(EntryClient.class);
 	}
 
